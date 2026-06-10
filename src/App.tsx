4a000fae598faa6ab2beb826c0d3755d7475defa
botirjon13 <tomama-uz@mail.ru>
2026-06-10 18:23:07 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Routes, Route } from 'react-router'
-import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
-  )
+  );
 }
