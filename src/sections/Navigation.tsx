e55315b3aec9f2af1df26a15c9d06ee5a8ce6f5a
botirjon13 <tomama-uz@mail.ru>
2026-06-11 11:43:38 +0500

    Update Navigation.tsx

--- src/sections/Navigation.tsx
+++ src/sections/Navigation.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import LanguageSwitcher from "../components/ui/LanguageSwitcher";
+import LanguageSwitcher from "../components/LanguageSwitcher";
 
 export default function Navigation() {
   const [scrolled, setScrolled] = useState(false);
