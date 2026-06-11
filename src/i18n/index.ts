2b65d76a5724fa4087300eaa51ce268cbd98944b
botirjon13 <tomama-uz@mail.ru>
2026-06-11 09:44:20 +0500

    Create index.ts

---
+++ src/i18n/index.ts
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import uz from "./locales/uz.json";
+import ru from "./locales/ru.json";
+import en from "./locales/en.json";
+
+const savedLanguage =
+  localStorage.getItem("language") || "uz";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      uz: { translation: uz },
+      ru: { translation: ru },
+      en: { translation: en },
+    },
+    lng: savedLanguage,
+    fallbackLng: "uz",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
