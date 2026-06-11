87a8f90d15b2fce2b9a653593aa5f9f94e188b9e
botirjon13 <tomama-uz@mail.ru>
2026-06-11 16:04:12 +0500

    Update Playground.tsx

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -2,7 +2,7 @@ export default function Playground() {
   return (
     <div className="w-full h-screen">
       <iframe
-        src="https://api-playground-backend-v8sd.onrender.com/docs"
+        src="https://api-playground-backend-v8sd.onrender.com/api/docs"
         title="Swagger"
         className="w-full h-full border-0"
       />
